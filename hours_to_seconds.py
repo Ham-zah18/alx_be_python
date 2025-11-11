1c986d60a955f225b38ec35700f24307b198df48
Busari Hamzah <habeebhamzah205@gmail.com>
2025-11-11 11:56:01 +0100

    Added four files

---
+++ hours_to_seconds.py
@@ -0,0 +1,5 @@
+# calculating how many seconds are there in 2 hours
+hours = 2
+number_of_seconds_in_1_hour = 3600
+seconds = hours * number_of_seconds_in_1_hour
+print(hours, "hour(s)", "is", seconds, "seconds" )
