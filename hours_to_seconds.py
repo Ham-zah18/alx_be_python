e8336bb580c9028b68c4b30d8fd90a6eeba8d0b8
hamzaBus.dev <149466818+Ham-zah18@users.noreply.github.com>
2025-11-11 12:05:47 +0100

    Delete hours_to_seconds.py

--- hours_to_seconds.py
+++
@@ -1,5 +0,0 @@
-# calculating how many seconds are there in 2 hours
-hours = 2
-number_of_seconds_in_1_hour = 3600
-seconds = hours * number_of_seconds_in_1_hour
-print(hours, "hour(s)", "is", seconds, "seconds" )
