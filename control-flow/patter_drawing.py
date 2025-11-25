cf2e0f3786a30ad8982fd9eaff7aecabd0142374
hamzaBus.dev <149466818+Ham-zah18@users.noreply.github.com>
2025-11-25 20:16:32 +0100

    Delete control-flow/patter_drawing.py

--- control-flow/patter_drawing.py
+++
@@ -1,9 +0,0 @@
-pattern = int(input("Enter the size of the pattern: "))
-
-count = 0
-
-while pattern > count:
-    for i in range(pattern):
-        print("*", end="")
-    print()  # Move to a new line after each row of asterisks
-    count += 1
