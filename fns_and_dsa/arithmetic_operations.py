daa23c2009856eb6716dc9ba57cc5f777232df50
hamzaBus.dev <149466818+Ham-zah18@users.noreply.github.com>
2025-12-04 15:43:39 +0100

    Update perform_operation to handle division errors
    Fix division by zero error handling and improve error message.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,11 +7,12 @@ def perform_operation(num1, num2, operation):
         return num1 * num2
     elif operation == "divide":
         if num2 == o:
-            return Zero
+            return Error("cannot divide by 0")
         else:
             return num1 / num2
     else:
 
         return "select a valid operation(add, subtract, multiply, division)"
+
 
 
