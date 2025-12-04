acc414d878cf72c6ad016cb5c5c60e1f6a0da332
hamzaBus.dev <149466818+Ham-zah18@users.noreply.github.com>
2025-12-04 15:34:47 +0100

    Add files via upload

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        return num1 / num2
+        if num2 == o:
+            return "cannot divide by 0"
+        else:
+            return num1 / num2
+    else:
+        return "select a valid operation(add, subtract, multiply, division)"
