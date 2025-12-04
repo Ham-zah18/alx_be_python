6e2ac5431043c1af61f37a224c18c52ee563dfa4
hamzaBus.dev <149466818+Ham-zah18@users.noreply.github.com>
2025-12-04 15:20:55 +0100

    Refactor perform_operation to return values
    Refactor perform_operation to return results instead of printing. Raise an error for division by zero.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,19 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
-        print(num1 + num2)
+        return(num1 + num2)
     elif operation == "subtract":
-        print(num1 - num2)
+        return(num1 - num2)
     elif operation == "multiply":
-        print(num1 * num2)
+        return(num1 * num2)
     elif operation == "divide":
         if num2 == o:
-            return ("cannot divide by 0")
+            raise("cannot divide by 0")
         else:
-            print(num1 / num2)
+            return(num1 / num2)
     else:
-        print("select a valid operation(add, subtract, multiply, division)")
+        return("select a valid operation(add, subtract, multiply, division)")
+
+perform_operation(num1, num2, operation)
+
 
 
