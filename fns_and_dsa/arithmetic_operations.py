c39a332a9315d0baf343f39b87cb780391f27ea1
hamzaBus.dev <149466818+Ham-zah18@users.noreply.github.com>
2025-12-04 15:22:39 +0100

    Refactor perform_operation function
    Refactor perform_operation function for clarity and fix errors.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,19 +1,20 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
-        return(num1 + num2)
+        return num1 + num2
     elif operation == "subtract":
-        return(num1 - num2)
+        return num1 - num2
     elif operation == "multiply":
-        return(num1 * num2)
+        return num1 * num2
     elif operation == "divide":
         if num2 == o:
-            raise("cannot divide by 0")
+            raise error("cannot divide by 0")
         else:
-            return(num1 / num2)
+            return num1 / num2
     else:
         return("select a valid operation(add, subtract, multiply, division)")
 
 perform_operation(num1, num2, operation)
+
 
 
 
