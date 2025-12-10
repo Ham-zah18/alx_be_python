79c00b27dd802397ae8f81ec400fa4d8cf5c06c4
hamzaBus.dev <149466818+Ham-zah18@users.noreply.github.com>
2025-12-10 13:21:08 +0100

    Clarify input prompt for future date calculation
    Updated input prompt for future date calculation to clarify the request.

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,10 +7,11 @@ def display_current_datetime():
 display_current_datetime()
 
 def calculate_future_date():
-    number_of_days = int(input("Enter a number of days: "))
+    number_of_days = int(input("Enter the number of days to add to the current date: "))
     future_date = datetime.now() + timedelta(days=number_of_days)
     formatted_future = future_date.strftime("%Y-%m-%d %H:%M:%S")
     print("future date:", formatted_future)
 
 calculate_future_date()
+
 
