7565a46c2349ab6a93e9e095042c2b5ae0d0a49e
hamzaBus.dev <149466818+Ham-zah18@users.noreply.github.com>
2025-12-26 13:33:44 +0100

    Fix formatting of display_balance method

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,5 +14,6 @@ class BankAccount:
 
     def display_balance(self):
 
-        print(f"Current Balance: ${self.account_balance: .00}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
+
 
