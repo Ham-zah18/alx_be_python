38bb76076dfcf1449c6231cf3e59079c431e4898
hamzaBus.dev <149466818+Ham-zah18@users.noreply.github.com>
2025-12-26 14:05:03 +0100

    Fix typo in division result message format

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,13 +1,14 @@
 def safe_divide(numerator, denominator):
     try:
         result = float(numerator)/float(denominator)
-        return result
+        return f"The reusult of the division is {result}
 
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
     else:
-        return "The result of the division is {result:.1f}"
+        return f"The result of the division is {result}"
+
 
 
